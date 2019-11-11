c5ba5af7cd3d8b4889b682854266c1f925050d54
sehmusaydogdu <sehmusaydogdu47@gmail.com>
2019-11-11 23:12:24 +0300

    regressogram added

--- NonparametricRegression.R
+++ NonparametricRegression.R
@@ -9,29 +9,28 @@ y_train <- as.matrix(data_y)[0:150,]
 x_test <- as.matrix(data_x)[151:272,]
 y_test <- as.matrix(data_y)[151:272,]
 
-N         <- length(y_train)
 bin_width <- 0.37
 origin    <- 1.5
 
-point_colors <- c("blue", "red")
+data_interval <- seq(from = min(x_train), to = max(x_train), by = 0.01)
 
-minimum_value <- -8
-maximum_value <- +8
+left_borders  <- seq(from = origin, to = max(x_train), by = bin_width)
+right_borders <- seq(from = origin+bin_width, to = max(x_train) + bin_width, by = bin_width)
 
-data_interval <- seq(from = minimum_value, to = maximum_value, by = 0.01)
+p_head <- sapply(1:length(left_borders), function(b) {
+  insideTheBox <- left_borders[b] < x_train & x_train <= right_borders[b]
+  sum(insideTheBox * y_train) / sum(insideTheBox)
+})
 
-left_borders  <- seq(from = minimum_value, to = maximum_value - bin_width, by = bin_width)
-right_borders <- seq(from = minimum_value + bin_width, to = maximum_value, by = bin_width)
-
-p_head <- sapply(1:length(left_borders), function(b) {sum(left_borders[b] < x_train & x_train <= right_borders[b])}) / (N * bin_width)
-
-
-plot(x_train, y_train,  pch = 19,lwd=1,
+plot(x_train, y_train, pch = 19,
      xlim = c(min(x_train), max(x_train)),
      ylim = c(min(y_train), max(y_train)),
      ylab = "Waiting time to next encruption (min)", 
      xlab = sprintf("Eruption time (min)", bin_width), 
-     main = sprintf("h = %g", bin_width), col = c("red", "blue"))
+     main = sprintf("h = %g", bin_width),
+     col = "blue")
+
+points(x_test,y_test, pch = 19,col = "red")
 
 for (b in 1:length(left_borders)) {
   lines(c(left_borders[b], right_borders[b]), c(p_head[b], p_head[b]), lwd = 2, col = "black")
@@ -39,4 +38,11 @@ for (b in 1:length(left_borders)) {
     lines(c(right_borders[b], right_borders[b]), c(p_head[b], p_head[b + 1]), lwd = 2, col = "black") 
   }
 }
+
+y_test_prediction <- sapply(1:length(x_test), function(b) {p_head[((x_test[b] - origin) / bin_width) + 1]})
+
+rmse <- sqrt(sum((y_test - y_test_prediction)*(y_test - y_test_prediction)) / length(y_test))
+print(sprintf("Regressogram => RMSE is %g when h is %g", rmse ,bin_width))
+
+###############################################################################################
 
